a7ec0c8e51d7a0f547c1fe59e0e8df7a69bdbaf3
Agung Nawawi <nawawiagung354@gmail.com>
2026-04-15 21:00:18 +0700

    add websocket

---
+++ client/client.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+async def send_message():
+    uri = "ws://localhost:8765"
+
+    async with websockets.connect(uri) as websocket:
+        print("Connected to server")
+
+        while True:
+            message = await asyncio.to_thread(input, "Ketik pesan: ")
+            await websocket.send(message)
+
+asyncio.run(send_message())
